863f6ce0be735a7b78d20f5d13715cc202354625
Loïc Dupin <loic.dupin1@gmail.com>
2023-06-14 16:08:34 +0200

    Mise a jour

---
+++ planty/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+        <footer id="site-footer" class="header-footer-group">
+			<img src="wp-content\themes\planty\Mask group.png" alt="" class="center-image">
+            <a href="#" class="noborder-color">Mentions légales</a>
+        </footer><!-- #site-footer -->
+
+      <?php wp_footer(); ?>
+
+   </body>
+</html>
