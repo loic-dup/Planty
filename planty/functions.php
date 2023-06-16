8cb7ef91796542cffa7a0e6452cb1ae5e88e6d4e
Loïc Dupin <loic.dupin1@gmail.com>
2023-06-16 11:26:56 +0200

    Ajout du hook permettant l'affichage de l'onglet Admin

--- planty/functions.php
+++ planty/functions.php
@@ -7,3 +7,10 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 /*HOOKS*/
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in()) {
+        $items .= '<li><a href="http://localhost/Planty/wp-admin/index.php" class="admin-position">Admin</a></li>';
+    }
+    return $items;
+}
