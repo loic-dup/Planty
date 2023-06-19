f2f05cc595098b0806cf1f2bb1d9f07e16d3b52b
Loïc Dupin <loic.dupin1@gmail.com>
2023-06-19 14:56:59 +0200

    Ajout du template acceuil+ class background-image

---
+++ planty/templates/template-acceuil.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Template Acceuil
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php
+get_footer();
